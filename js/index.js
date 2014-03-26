2f6cf49518622dea4969ef9d338287c9f0a52885
Amit Metkar <amit.metkar@gmail.com>
2014-03-26 16:27:18 -0500

    Adding project

---
+++ js/index.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+
+    $("#jquery").box({ "height": "300px" });
+    $("#javascript").box({ "height": "300px" }).hide();
+
+    $("#jqueryCollapsed").box({ "height": "30px" }).hide();
+    $("#javascriptCollapsed").box({ "height": "30px" }).hide();
+
+    $("#jqup").box({ "height": "30px", "border-top-width": "0px", "float": "left" });
+    $("#jsup").box({ "height": "30px", "border-top-width": "0px", "float": "left" }).hide();
+
+    $("#jquery #btntrigger").click(function () {
+        $.event.trigger({
+            type: "TextAvailable",
+            message: $("#jquery #txtText").val()
+        });
+    });
+
+    $(document).on("TextAvailable", function (evt) {
+        var text = "<li>" + evt.message + "</li>";
+        $("#jquery ul").prepend(text);
+        $("#javascript").show("fast");
+        $("#jsup").show();
+    });
+
+    $("#javascript #btntrigger").click(function () {
+        document.dispatchEvent(new CustomEvent("TextAvailable1", { "detail": { "message": $("#javascript #txtText").val()} }));
+    });
+
+    document.addEventListener("TextAvailable1", function (e) {
+        var text = "<li>" + e.detail.message + "</li>";
+        $("#javascript ul").prepend(text);
+    });
+
+    $("#jqueryCollapsed #btnDown").click(function () {
+        $("#jqueryCollapsed").hide();
+        $("#jquery").css("height", "30px").show();
+        $("#jqup").show();
+        $("#jquery").animate({ height: "300px", opacity: 100 }, { queue: false, duration: 500 });
+    });
+
+    $("#javascriptCollapsed #btnDown").click(function () {
+        $("#javascriptCollapsed").hide();
+        $("#javascript").css("height", "30px").show();
+        $("#jsup").show();
+        $("#javascript").animate({ height: "300px", opacity: 100 }, { queue: false, duration: 500 });
+    });
+
+    $("#jqup #btnUp").click(function () {
+        $("#jquery").animate({ height: "0px", opacity: 0 }, { queue: false, duration: 500, complete: function () {
+            $("#jquery").hide();
+            $("#jqup").hide();
+            $("#jqueryCollapsed").show();
+        }
+        });
+    });
+
+    $("#jsup #btnUp").click(function () {
+        $("#javascript").animate({ height: "0px", opacity: 0 }, { queue: false, duration: 500, complete: function () {
+            $("#javascript").hide();
+            $("#jsup").hide();
+            $("#javascriptCollapsed").show();
+        }
+        });
+    });
+});
---
+++ plugins/box.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+    $.fn.box = function (css) {
+        this.css({
+            "background-color": "#CCDADB",
+            "border-color": "gray",
+            "border-width": 2,
+            "border-style": "solid",
+            "padding": "15px",
+            "width": "375px",
+            "overflow": "auto"
+        });
+        if (css) {
+            this.css(css);
+        }
+        return this;
+    }
+})(jQuery);
