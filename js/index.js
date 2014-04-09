4472ee9dee56afbb29107724946e698768582821
Amit Metkar <amit.metkar@gmail.com>
2014-04-09 15:55:27 -0500

    Change in index and added Knockoutjs example

--- js/index.js
+++ js/index.js
@@ -12,19 +12,24 @@
     $("#jquery #btntrigger").click(function () {
         $.event.trigger({
             type: "TextAvailable",
-            message: $("#jquery #txtText").val()
+            eventData: $("#jquery #txtText").val()
         });
     });
 
     $(document).on("TextAvailable", function (evt) {
-        var text = "<li>" + evt.message + "</li>";
+        var text = "<li>" + evt.eventData + "</li>";
         $("#jquery ul").prepend(text);
+    });
+
+    $("#btnjs").click(function () {
         $("#javascript").show("fast");
         $("#jsup").show();
+        $(this).attr("disabled", "false");
     });
 
     $("#javascript #btntrigger").click(function () {
-        document.dispatchEvent(new CustomEvent("TextAvailable1", { "detail": { "message": $("#javascript #txtText").val()} }));
+        var event = new CustomEvent("TextAvailable1", { "detail": { "message": $("#javascript #txtText").val()} });
+        document.dispatchEvent(event);
     });
 
     document.addEventListener("TextAvailable1", function (e) {
@@ -34,14 +39,14 @@
 
     $("#jqueryCollapsed #btnDown").click(function () {
         $("#jqueryCollapsed").hide();
-        $("#jquery").css("height", "30px").show();
+        $("#jquery").css({ "height": "30px" }).show();
         $("#jqup").show();
         $("#jquery").animate({ height: "300px", opacity: 100 }, { queue: false, duration: 500 });
     });
 
     $("#javascriptCollapsed #btnDown").click(function () {
         $("#javascriptCollapsed").hide();
-        $("#javascript").css("height", "30px").show();
+        $("#javascript").css({ "height": "30px", "opacity": "0.5" }).show();
         $("#jsup").show();
         $("#javascript").animate({ height: "300px", opacity: 100 }, { queue: false, duration: 500 });
     });
@@ -63,4 +68,18 @@
         }
         });
     });
+
+    //Drawer code
+    $("#Div1").box({ "height": "300px" });
+    $("#Div2").box({ "height": "30px", "border-top-width": "0px", "float": "left" });
+
+    $("#Div1").kendoMobileDrawer({
+        container: "#Div2"
+    });
+
+    $("#Button2").click(function () {
+        $("#Div1").data("kendoMobileDrawer").hide();
+        window.setInterval(function () { $("#Div1").data("kendoMobileDrawer").show(); }, 5000);
+    });
+    //Drawer code
 });
