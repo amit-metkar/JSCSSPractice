e781826e98ce5405d5038d774df51d641a2cb898
amit-metkar <amit.metkar@gmail.com>
2014-04-24 17:57:00 -0500

    adding appointment

---
+++ js/appointment.js
@@ -0,0 +1,15 @@
+﻿var Appointment = Backbone.Model.extend({});
+var appointment = new Appointment({ title: "My Appointment" });
+var AppointmentView = Backbone.View.extend({
+    render: function () {
+        var html = "<li>" + this.model.get('title') + "</li>";
+        $(this.el).html(html);
+    },
+    model: appointment
+});
+//var appointmentView = new AppointmentView();
+
+appointment.set('title', 'My Backbone Hurts');
+
+AppointmentView.render();
+$('#app').html(AppointmentView.el);
